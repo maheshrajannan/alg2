de6ed6e0959f1859aa02e5fa25468330061f4212
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2015-11-17 20:41:15 -0600

    initial

---
+++ alg.artifact/src/main/java/alg/group/alg/artifact/App.java
@@ -0,0 +1,13 @@
+package alg.group.alg.artifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
