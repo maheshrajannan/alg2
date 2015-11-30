cb7c732b7d5d8d849673917b877abaccf402726f
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2015-11-29 18:55:17 -0600

    configured log4j to print to console.

--- alg.artifact/src/test/java/alg/group/alg/artifact/NodeTest.java
+++ alg.artifact/src/test/java/alg/group/alg/artifact/NodeTest.java
@@ -5,6 +5,8 @@ package alg.group.alg.artifact;
 
 import static org.junit.Assert.*;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -16,12 +18,16 @@ import org.junit.Test;
  *
  */
 public class NodeTest {
-
+    // Define a static logger variable so that it references the
+    // Logger instance named "MyApp".
+    private static final Logger logger = LogManager.getLogger(NodeTest.class);
+    
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		logger.debug("Testing node as a POJO");
 	}
 
 	/**
@@ -29,6 +35,7 @@ public class NodeTest {
 	 */
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		logger.debug("DONE Testing node as a POJO");
 	}
 
 	/**
@@ -50,6 +57,7 @@ public class NodeTest {
 	 */
 	@Test
 	public final void testEqualsObject() {
+		logger.debug("testing if 2 nodes are equal");
 		Node<Integer> root = new Node<Integer>(2);
 		root.setLeft(7);
 		root.setRight(5);
@@ -63,6 +71,7 @@ public class NodeTest {
 	 */
 	@Test
 	public final void testEqualsObjectNoLeft() {
+		logger.debug("testing if 2 nodes, with no left are equal");
 		Node<Integer> root = new Node<Integer>(2);
 		root.setRight(5);
 		Node<Integer> root2 = new Node<Integer>(2);
@@ -74,6 +83,7 @@ public class NodeTest {
 	 */
 	@Test
 	public final void testEqualsObjectNoRight() {
+		logger.debug("testing if 2 nodes with no right are equal");
 		Node<Integer> root = new Node<Integer>(2);
 		root.setLeft(7);
 		Node<Integer> root2 = new Node<Integer>(2);
@@ -85,6 +95,7 @@ public class NodeTest {
 	 */
 	@Test
 	public final void testEqualsObjectNoLeftAndRight() {
+		logger.debug("testing if 2 nodes with no left and no right are equal");
 		Node<Integer> root = new Node<Integer>(2);
 		Node<Integer> root2 = new Node<Integer>(2);
 		assertTrue("it is supposed to be equal"+ root +"" + root2,root.equals(root2));
@@ -94,6 +105,7 @@ public class NodeTest {
 	 */
 	@Test
 	public final void testNotEqualsObject() {
+		logger.debug("ensuring 2 nodes with different left and right are not equal");
 		Node<Integer> root = new Node<Integer>(2);
 		root.setRight(7);
 		root.setLeft(5);
@@ -107,6 +119,7 @@ public class NodeTest {
 	 */
 	@Test
 	public final void testLeftEqualsLeft() {
+		logger.debug("ensuring 2 nodes have same left");
 		Node<Integer> root = new Node<Integer>(2);
 		root.setLeft(7);
 		root.setRight(1);
@@ -120,6 +133,7 @@ public class NodeTest {
 	 */
 	@Test
 	public final void testLeftEqualsLeftNull() {
+		logger.debug("ensuring 2 nodes have same left, with left being null");
 		Node<Integer> root = new Node<Integer>(2);
 		root.setRight(1);
 		Node<Integer> root2 = new Node<Integer>(2);
@@ -131,6 +145,7 @@ public class NodeTest {
 	 */
 	@Test
 	public final void testNotLeftEqualsLeft() {
+		logger.debug("ensuring 2 nodes do NOT have same left");
 		Node<Integer> root = new Node<Integer>(2);
 		root.setLeft(7);
 		root.setRight(1);
@@ -144,6 +159,7 @@ public class NodeTest {
 	 */
 	@Test
 	public final void testRightEqualsRight() {
+		logger.debug("ensuring 2 nodes have same right");
 		Node<Integer> root = new Node<Integer>(2);
 		root.setLeft(7);
 		root.setRight(1);
@@ -157,6 +173,7 @@ public class NodeTest {
 	 */
 	@Test
 	public final void testRightEqualsRightNull() {
+		logger.debug("ensuring 2 nodes have same right, with right being null");
 		Node<Integer> root = new Node<Integer>(2);
 		root.setLeft(7);
 		Node<Integer> root2 = new Node<Integer>(2);
@@ -168,6 +185,7 @@ public class NodeTest {
 	 */
 	@Test
 	public final void testNotRightEqualsRight() {
+		logger.debug("ensuring 2 nodes do NOT have same right");
 		Node<Integer> root = new Node<Integer>(2);
 		root.setLeft(7);
 		root.setRight(1);
